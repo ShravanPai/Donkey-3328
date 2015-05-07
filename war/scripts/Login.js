0f4c529613f662cee2f071da6c17c3f472097b0c
Shravan Pai <glxpai@gmail.com>
2015-05-07 01:52:35 -0700

    CODE: Added cookie support

--- war/scripts/Login.js
+++ war/scripts/Login.js
@@ -1,12 +1,33 @@
-var LoginApp = angular.module('LoginApp',[]);
+var LoginApp = angular.module('LoginApp',['ngCookies']);
   
-LoginApp.controller('LoginController', function($scope, $http) {
+LoginApp.controller('LoginController', function($scope, $http, $cookieStore) {
 	$scope.message = '';
 	$scope.userName = '';
 	$scope.loginSuccess = false;
 	$scope.gameInProgress = false;
 	$scope.isHost = false;
+	$scope.gameInfo = {};
+	$scope.initParams = function() {
+    	if (!angular.isUndefined($cookieStore.get('gameInfo'))) {
+    		$scope.message = 'You are already in the game as ' + $cookieStore.get('gameInfo').userName;
+    		$scope.isHost = $cookieStore.get('gameInfo').isHost;
+    		$scope.loginSuccess = $cookieStore.get('gameInfo').loginSuccess;
+    		console.log($scope.loginSuccess);
+    	}		
+	};
     $scope.login = function() {
+    	$scope.gameInfo.userName = $scope.userName;
+    	$scope.gameInfo.isHost = $scope.isHost;
+    	$scope.gameInfo.gameInProgress = $scope.gameInProgress;
+    	$scope.gameInfo.loginSuccess = $scope.loginSuccess;
+
+    	if (!angular.isUndefined($cookieStore.get('gameInfo'))) {
+    		$scope.message = 'You are already in the game as ' + $cookieStore.get('gameInfo').userName;
+    		$scope.isHost = $cookieStore.get('gameInfo').isHost;
+    		$scope.loginSuccess = $cookieStore.get('gameInfo').loginSuccess;
+    		return;
+    	}
+    		
       $http.get('http://donkey-3328.appspot.com/game/hello/'+$scope.userName+'/').
     		success(function(data) {
     			if (data.indexOf('Please select a unique name') > -1) {
@@ -14,10 +35,15 @@ LoginApp.controller('LoginController', function($scope, $http) {
     				return;
     			} else if (data.indexOf('you are the host') > -1) {
     				$scope.isHost = true;
-    			} else if (data.indexOf('game in progress') > -1)
+    				$scope.gameInfo.isHost = true;
+    			} else if (data.indexOf('game in progress') > -1) {
     				$scope.gameInProgress = true;
+    			}
         		$scope.message = data;
         		$scope.loginSuccess = true;
+        		$scope.gameInfo
+        		$cookieStore.put('gameInfo', $scope.gameInfo);
+        		console.log('Cookie value : ' + $cookieStore.get('gameInfo').isHost);
         	}).
         	error(function(data, status, headers, config){
         		$scope.message = 'Error in getting response : ' + data;
@@ -26,6 +52,7 @@ LoginApp.controller('LoginController', function($scope, $http) {
     $scope.endGame = function() {
     	$http.get('http://donkey-3328.appspot.com/game/end_game/').
 		success(function(data) {
+			$cookieStore.remove('gameInfo');
 			$scope.message = data;
 			$scope.isHost = false;
     	}).
