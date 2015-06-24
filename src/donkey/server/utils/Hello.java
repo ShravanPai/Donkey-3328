f821f11af73e9820acde66a2554b843aec25f1bb
Shravan Pai <glxpai@gmail.com>
2015-06-23 22:54:03 -0700

    CODE : Removing unused import

--- src/donkey/server/utils/Hello.java
+++ src/donkey/server/utils/Hello.java
@@ -17,8 +17,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import jsontemplates.BaseJSON;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
